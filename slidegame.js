803cdc1f065b29b37ab3f625d5db2d934f4d8763
Brett Stine <r.brettstine@gmail.com>
2024-01-23 11:01:07 -0500

    Add right-slide-to-obstacle logic. GAME IS BROKEN
     - This logic needs to be applied to all directions immediately
     - Just wanted to commit this now while one direction's logic is
        functioning properly

--- slidegame.js
+++ slidegame.js
@@ -1,35 +1,51 @@
 
-//GENERATE PLAYER ELEMENT ON BOARD
+//GENERATE PLAYER ELEMENT AND NODE ON BOARD
 
-const player = document.createElement("div");
-let playerStyle = player.style;
+const playerNode = document.createElement("div");
+let playerNodeStyle = playerNode.style;
 let gameContainer = document.getElementById("game-container");
 
-player.classList.add('player');
-gameContainer.appendChild(player);
+playerNode.classList.add('player');
+gameContainer.appendChild(playerNode);
 
+//create player object for backend logic that changes based on keypress.
+//playerNode (visual representation) will update based on changes to backend object!
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+const player = new Player(0,0);
 
 //BOARD GENERATION
 
 const BOARD_SIZE = 10;
-let countObstacles = 5;
+let countObstacles = 10;
 let obstacleCoordinates = [];
 let board = Array(BOARD_SIZE).fill().map(() => Array(BOARD_SIZE).fill(0))
 
 function generateRandomBoard() {
   let currentObstacles = countObstacles;
   while (currentObstacles > 0) {
-    let obstaclePosX = Math.ceil(Math.random()*8)+1;
-    let obstaclePosY = Math.ceil(Math.random()*8)+1;
-    console.log(obstaclePosX);
+    let obstaclePosX = Math.floor(Math.random()*10);
+    let obstaclePosY = Math.floor(Math.random()*10);
     if (board[obstaclePosX][obstaclePosY] === 0) {
       board[obstaclePosX][obstaclePosY] = 1;
       currentObstacles -= 1;
-      obstacleCoordinates.push([obstaclePosX,obstaclePosY]);
+      obstacleCoordinates.push([obstaclePosY,obstaclePosX]);
     }
   }
-  console.log(board);
-  console.log(obstacleCoordinates);
+  obstacleCoordinates.sort(sortFunction);
+}
+
+function sortFunction(a, b) {
+    if (a[0] === b[0]) {
+        return 0;
+    }
+    else {
+        return (a[0] < b[0]) ? -1 : 1;
+    }
 }
 
 function updateBoardDisplay() {
@@ -60,23 +76,34 @@ function updatePositionByPercentage(positionAttribute, amountPercentage) {
   return newPositionAttributeValue;
 }
 
+function updatePositionToCoordinates(positionAttribute, coordinate) {
+  return positionAttribute = coordinate*10 + "%";
+}
+
 function handleKeyDownEvent(key) {
   switch (key) {
     case "ArrowLeft":
         // Left pressed
-        playerStyle.left = updatePositionByPercentage(playerStyle.left, -10);
+        playerNodeStyle.left = updatePositionByPercentage(playerNodeStyle.left, -10);
         break;
     case "ArrowRight":
         // Right pressed
-        playerStyle.left = updatePositionByPercentage(playerStyle.left, 10);
+        let targetCoordinates = 
+                      (obstacleCoordinates.filter((xyPair)=>(xyPair[1]===player.y)&&(xyPair[0]>player.x))
+                                          .sort((a,b) => (a[0]>b[0]) ? 1 : -1)
+                                          [0]);
+        let destinationX = targetCoordinates[0]-1;
+        player.x = destinationX;
+        playerNodeStyle.left = updatePositionToCoordinates(playerNodeStyle.left, destinationX);
         break;
     case "ArrowUp":
         // Up pressed
-        playerStyle.top = updatePositionByPercentage(playerStyle.top, -10);
+        playerNodeStyle.top = updatePositionByPercentage(playerNodeStyle.top, -10);
         break;
     case "ArrowDown":
         // Down pressed
-        playerStyle.top = updatePositionByPercentage(playerStyle.top, 10);
+        player.y += 1;
+        playerNodeStyle.top = updatePositionByPercentage(playerNodeStyle.top, 10);
         break;
   }
 }
