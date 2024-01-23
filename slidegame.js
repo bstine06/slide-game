d7be953166b049905ba8776d2033602732ae59ed
Brett Stine <r.brettstine@gmail.com>
2024-01-22 22:44:48 -0500

    Implement random gen & display of obstacles
     - Change position attribute of game container to relative:
         this allows absolute positioning of all gameplay elements within
         the context of the game container

--- slidegame.js
+++ slidegame.js
@@ -1,10 +1,55 @@
 
+//GENERATE PLAYER ELEMENT ON BOARD
+
 const player = document.createElement("div");
 let playerStyle = player.style;
 let gameContainer = document.getElementById("game-container");
 
 player.classList.add('player');
 gameContainer.appendChild(player);
+
+
+//BOARD GENERATION
+
+const BOARD_SIZE = 10;
+let countObstacles = 5;
+let obstacleCoordinates = [];
+let board = Array(BOARD_SIZE).fill().map(() => Array(BOARD_SIZE).fill(0))
+
+function generateRandomBoard() {
+  let currentObstacles = countObstacles;
+  while (currentObstacles > 0) {
+    let obstaclePosX = Math.ceil(Math.random()*8)+1;
+    let obstaclePosY = Math.ceil(Math.random()*8)+1;
+    console.log(obstaclePosX);
+    if (board[obstaclePosX][obstaclePosY] === 0) {
+      board[obstaclePosX][obstaclePosY] = 1;
+      currentObstacles -= 1;
+      obstacleCoordinates.push([obstaclePosX,obstaclePosY]);
+    }
+  }
+  console.log(board);
+  console.log(obstacleCoordinates);
+}
+
+function updateBoardDisplay() {
+  for (let i=0; i<countObstacles; i++) {
+    let obstacle = document.createElement("div");
+    obstacle.classList.add("obstacle");
+    obstacle.style.left = obstacleCoordinates[i][0]*10+"%";
+    obstacle.style.top = obstacleCoordinates[i][1]*10+"%";
+    gameContainer.appendChild(obstacle);
+  }
+}
+
+
+
+
+generateRandomBoard();
+updateBoardDisplay();
+
+
+//HANDLE KEYPRESSES
 
 document.addEventListener('keydown', function(event) {
   handleKeyDownEvent(event.key);
