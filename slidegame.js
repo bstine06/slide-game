9c3c0f44a21c2068774808c8fa2fcc521bdc5b6d
Brett Stine <r.brettstine@gmail.com>
2024-01-24 19:24:04 -0500

    Implement "explosions" - obstacles can be erased
     - all obstacles within 1 block radius of player are eradicated when
        explosion is triggered
     - still needs some kind of animation

--- slidegame.js
+++ slidegame.js
@@ -11,77 +11,86 @@ class Board {
   constructor(size, countObstacles) {
     this.size = size;
     this.countObstacles = countObstacles;
-    this.boardArr = Array(this.size).fill().map(() => Array(this.size).fill(0))
-    this.obstacleCoordinates = [];
+    this.obstacles = [];
     this.player = new Item(size, "player");
     this.finish = new Item(size, "finish");
     this.resetToThisState;
   }
   generateRandomBoard() {
     for (let i=0; i<this.countObstacles; i++) {
-      let obstaclePosX = Math.floor(Math.random()*this.size);
-      let obstaclePosY = Math.floor(Math.random()*this.size);
-      if (this.boardArr[obstaclePosX][obstaclePosY] === 0) {
-        this.boardArr[obstaclePosX][obstaclePosY] = 1;
-        this.obstacleCoordinates.push([obstaclePosY,obstaclePosX]);
-      } else {
-        i--;
-      }
+      let newObstacle = new Item(this.size, "obstacle");
+      newObstacle.setXY(this.findRandomUnoccupiedCoordinates());
+      this.obstacles.push(newObstacle);
     }
     this.player.setXY(this.findRandomUnoccupiedCoordinates());
     this.finish.setXY(this.findRandomUnoccupiedCoordinates());
-    this.resetToThisState = [this.obstacleCoordinates, this.player.getXY(), this.finish.getXY()];
+    this.resetToThisState = [this.obstacles, this.player.getXY(), this.finish.getXY()];
   }
-  updateBoardDisplay() {
-    for (let i=0; i<this.countObstacles; i++) {
-      let obstacle = document.createElement("div");
-      obstacle.classList.add("obstacle");
-      obstacle.style.left = this.obstacleCoordinates[i][0]*(100/this.size)+"%";
-      obstacle.style.top = this.obstacleCoordinates[i][1]*(100/this.size)+"%";
-      obstacle.style.width = (100/this.size)+"%";
-      obstacle.style.height = (100/this.size)+"%";
-      gameDomElement.appendChild(obstacle);
-    }
-  }
+  // updateBoardDisplay() {
+  //   for (let i=0; i<this.obstacles.length; i++) {
+  //     let obstacle = document.createElement("div");
+  //     obstacle.classList.add("obstacle");
+  //     obstacle.style.left = this.getObstacleXYs()[i][0]*(100/this.size)+"%";
+  //     obstacle.style.top = this.getObstacleXYs()[i][1]*(100/this.size)+"%";
+  //     obstacle.style.width = (100/this.size)+"%";
+  //     obstacle.style.height = (100/this.size)+"%";
+  //     gameDomElement.appendChild(obstacle);
+  //   }
+  // }
   findRandomUnoccupiedCoordinates() {
     let randomX = 0;
     let randomY = 0;
     do {
       randomX = Math.floor(Math.random()*this.size);
       randomY = Math.floor(Math.random()*this.size);
-    } while (this.obstacleCoordinates.filter((e)=>(e[0]===randomX && e[1]===randomY)).length!==0);
+    } while (this.getObstacleXYs().filter((e)=>(e[0]===randomX && e[1]===randomY)).length!==0);
     return [randomX,randomY];
+  }
+  getObstacleXYs(){
+    return this.obstacles.map(o => o.getXY());
   }
 
   reset(){
-    this.obstacleCoordinates = this.resetToThisState[0];
+    this.obstacles = this.resetToThisState[0];
     this.player.setXY(this.resetToThisState[1]);
     this.finish.setXY(this.resetToThisState[2]);
   }
 
   movePlayerLeft(){
-    this.player.setX(this.obstacleCoordinates.filter(xy => xy[1] === this.player.getY() && xy[0] < this.player.getX())
+    this.player.setX(this.getObstacleXYs().filter(xy => xy[1] === this.player.getY() && xy[0] < this.player.getX())
                                                 .concat([[-1, this.player.getY()]])
                                                 .sort((a,b) => (a[0]<b[0]) ? 1 : -1)
                                                 [0][0]+1);
   }
   movePlayerRight(){
-    this.player.setX(this.obstacleCoordinates.filter(xy => xy[1] === this.player.getY() && xy[0] > this.player.getX())
+    this.player.setX(this.getObstacleXYs().filter(xy => xy[1] === this.player.getY() && xy[0] > this.player.getX())
                                                 .concat([[this.size, this.player.getY()]])
                                                 .sort((a,b) => (a[0]>b[0]) ? 1 : -1)
                                                 [0][0]-1);
   }
   movePlayerUp(){
-    this.player.setY(this.obstacleCoordinates.filter(xy => xy[0] === this.player.getX() && xy[1] < this.player.getY())
+    this.player.setY(this.getObstacleXYs().filter(xy => xy[0] === this.player.getX() && xy[1] < this.player.getY())
                                                 .concat([[this.player.getX(),-1]])
                                                 .sort((a,b) => (a[1]<b[1]) ? 1 : -1)
                                                 [0][1]+1);
   }
   movePlayerDown(){
-    this.player.setY(this.obstacleCoordinates.filter(xy => xy[0] === this.player.getX() && xy[1] > this.player.getY())
+    this.player.setY(this.getObstacleXYs().filter(xy => xy[0] === this.player.getX() && xy[1] > this.player.getY())
                                                 .concat([[this.player.getX(),this.size]])
                                                 .sort((a,b) => (a[1]>b[1]) ? 1 : -1)
                                                 [0][1]-1);
+  }
+  triggerExplosion(){
+    let xRange = [this.player.getX()-1, this.player.getX(), this.player.getX()+1];
+    let yRange = [this.player.getY()-1, this.player.getY(), this.player.getY()+1];
+    let elementsToDelete = (this.obstacles.filter(o => xRange.includes(o.getX()) && yRange.includes(o.getY())));
+    console.log(elementsToDelete);
+    elementsToDelete.forEach(item => {
+      // Remove the DOM element from its parent
+      item.node.parentNode.removeChild(item.node);
+      // Delete the JavaScript object
+    });
+    this.obstacles = (this.obstacles.filter(o => !(xRange.includes(o.getX()) && yRange.includes(o.getY()))));
   }
 }
 
@@ -147,6 +156,8 @@ function handleKeyDownEvent(key) {
     case "ArrowDown":
         myBoard.movePlayerDown();
         break;
+    case " ":
+        myBoard.triggerExplosion();
   }
 }
 
@@ -163,6 +174,5 @@ resetBtn.addEventListener("click", function() {
 
 let myBoard = new Board(20, 100);
 myBoard.generateRandomBoard();
-myBoard.updateBoardDisplay();
 
 
